0cd45997ebb009447513005ebcbe3c61521f975e
Tetsuya Idota <tidota@hawaii.edu>
2020-05-21 02:48:59 -1000

    Add a script for master/slave

---
+++ scripts/slave_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script setups a local machine running a gzclient accessing the master
+# which is running a gzserver and roscore
+export ROS_MASTER_URI=http://seilon-3:11311/
+export ROS_IP=`hostname -I | awk '{print $1}'`
+export ROS_HOSTNAME=$ROS_IP
+export GAZEBO_MASTER_URI=seilon-3
+export GAZEBO_IP=`hostname -I | awk '{print $1}'`
